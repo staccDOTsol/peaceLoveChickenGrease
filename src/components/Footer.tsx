e2ef7a4e7c826b5c61e38b16cec167d3a6f70a5e
azoth zephyr <azoth@zephyr.sx>
2023-03-18 23:51:02 -0600

    push footer down a bit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,9 @@
 import { FC } from 'react';
 import Link from 'next/link';
 
-import PublicIcon from '@mui/icons-material/Public';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import { RxDiscordLogo, RxTwitterLogo, RxInstagramLogo, RxGlobe } from 'react-icons/rx';
-
 export const Footer: FC = () => {
     return (
-        <div className="relative mb-20 mt-10">
+        <div className="relative mb-20 mt-60">
             <footer className="border-t-2 border-[#141414] bg-black hover:text-white absolute w-full" >
                 <div className="ml-12 py-12 mr-12">
                     <div className="grid grid-cols-1 md:grid-cols-1 gap-2 md:gap- md:space-x-12 relative">
