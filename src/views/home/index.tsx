131bc653b1241f03821e276ef2b09b1d138b5564
azoth zephyr <azoth@zephyr.sx>
2023-03-18 22:34:51 -0600

    change text

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -49,12 +49,12 @@ export const HomeView: FC = ({ }) => {
       <div className="md:hero-content flex flex-col">
         <div>
         <h1 className="text-center text-5xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-br from-green-500 to-fuchsia-100 mb-4">
-          Ordinal Millionaires Country Club
+          Ordinal Meerkats Country Club
         </h1>
         {/* </div>
         <div> */}
               <h4 className="text-2x1 md:text-2xl text-center text-slate-300 my-2">
-                <p>Pay to mint on Solana, receive your ordinal on BTC</p>
+                <p>Use Solana to mint and have your Ordinal sent to your BTC wallet!</p>
               </h4>
             </div>
         <div className="flex flex-col mt-2 center-items">
