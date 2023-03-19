5f4c936f9d82cfc5933945dd4ad434d84245f91e
azoth zephyr <azoth@zephyr.sx>
2023-03-18 20:16:37 -0600

    add function to calculate the transaction cost in sol

---
+++ src/utils/fees.tsx
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+async function calculateTransactionCostInSol(satoshisPerByte: number = 10, bytesPerTransaction: number = 25000): Promise<number | null> {
+  try {
+    // Fetch the SOL to BTC price from Kraken
+    const response = await axios.get('https://api.kraken.com/0/public/Ticker?pair=SOLXBT');
+    const solToBtcPrice = parseFloat(response.data.result.SOLXBT.c[0]);
+
+    // Calculate the total transaction cost in Bitcoin
+    const totalTransactionCostInBtc = satoshisPerByte * bytesPerTransaction * 0.00000001;
+
+    // Convert the transaction cost to Solana
+    const transactionCostInSol = totalTransactionCostInBtc / solToBtcPrice;
+
+    return transactionCostInSol;
+  } catch (error) {
+    console.error('Error fetching SOL to BTC price:', error);
+    return null;
+  }
+}
