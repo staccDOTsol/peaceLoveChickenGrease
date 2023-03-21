cb64bea82192d42c3c7453ea73ec2ec4bc1d6d6e
AlexMNet <emtamaldonado@gmail.com>
2023-03-20 20:12:21 -0700

    fix button styles, alignment and notifications on home view

--- src/components/SendTransaction.tsx
+++ src/components/SendTransaction.tsx
@@ -1,186 +1,218 @@
-import { useConnection, useWallet, } from '@solana/wallet-adapter-react';
-import { Connection, Keypair, PublicKey, SystemProgram, Transaction, TransactionMessage, TransactionSignature, VersionedTransaction } from '@solana/web3.js';
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import {
+  Connection,
+  Keypair,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+  TransactionMessage,
+  TransactionSignature,
+  VersionedTransaction,
+} from '@solana/web3.js';
 import { FC, useCallback, useContext, useEffect, useState } from 'react';
-import { notify } from "../utils/notifications";
+import { notify } from '../utils/notifications';
 import { FeeContext } from '../contexts/FeeContext';
-import * as anchor from '@coral-xyz/anchor'
-import { calculateTransactionCostInSol } from "../utils/fees";
+import * as anchor from '@coral-xyz/anchor';
+import { calculateTransactionCostInSol } from '../utils/fees';
 import { useUserContext } from 'contexts/UserContext';
 import { Metaplex } from '@metaplex-foundation/js';
 import axios from 'axios';
 export const SendTransaction: FC = () => {
-   const connection = new Connection("https://rpc.helius.xyz?api-key=8913a285-a5ef-4c35-8d80-03fb276eff2f");
-   const wallet = useWallet()
-   const {selectedFee} = useContext(FeeContext);
-   const {totalCost} = useContext(FeeContext);
-    const [isNftOwner, setIsNftOwner] = useState(false);
-   const [dummyLamports, setDummyLamports] = useState(0);
-    const metaplex = new Metaplex(new Connection("https://rpc.helius.xyz?api-key=8913a285-a5ef-4c35-8d80-03fb276eff2f"))
-    
-    const url = `https://api.helius.xyz/v1/mintlist?api-key=8913a285-a5ef-4c35-8d80-03fb276eff2f`
-useEffect(() => {
-    setTimeout(async() => {
-        
-let mks = []
-let found = false
-    if (!wallet.connected) 
-    {
-        if (found){
-            setDummyLamports(0.5 * 10 ** 9)
-          
+  const connection = new Connection(
+    'https://rpc.helius.xyz?api-key=8913a285-a5ef-4c35-8d80-03fb276eff2f'
+  );
+  const wallet = useWallet();
+  const { selectedFee } = useContext(FeeContext);
+  const { totalCost } = useContext(FeeContext);
+  const [isNftOwner, setIsNftOwner] = useState(false);
+  const [dummyLamports, setDummyLamports] = useState(0);
+  const metaplex = new Metaplex(
+    new Connection(
+      'https://rpc.helius.xyz?api-key=8913a285-a5ef-4c35-8d80-03fb276eff2f'
+    )
+  );
+
+  const url = `https://api.helius.xyz/v1/mintlist?api-key=8913a285-a5ef-4c35-8d80-03fb276eff2f`;
+  useEffect(() => {
+    setTimeout(async () => {
+      let mks = [];
+      let found = false;
+      if (!wallet.connected) {
+        if (found) {
+          setDummyLamports(0.5 * 10 ** 9);
+        } else {
+          setDummyLamports(2.5 * 10 ** 9);
         }
-        else {
-          setDummyLamports(2.5 * 10 ** 9)
-        }
-        return 
-    } 
-    let response = await   axios.post(url, {
-        "query": {
-            // ABC collection
-            "firstVerifiedCreators": ["HAKkYiokH32HbgUJFQUr4xLWNFdbQtEUzTMsW7H3fDTk"]
+        return;
+      }
+      let response = await axios.post(url, {
+        query: {
+          // ABC collection
+          firstVerifiedCreators: [
+            'HAKkYiokH32HbgUJFQUr4xLWNFdbQtEUzTMsW7H3fDTk',
+          ],
         },
-        "options": {
-            "limit": 10000
+        options: {
+          limit: 10000,
+        },
+      });
+      for (var d of response.data.result) {
+        mks.push(d.mint);
+      }
+
+      let r2 = await axios.post(url, {
+        query: {
+          // ABC collection
+          firstVerifiedCreators: [
+            '8mNmf15xNrMFQLNSNrHxxswy7a1NfaSFwXHkVUPeMWwU',
+          ],
+        },
+        options: {
+          limit: 10000,
+        },
+      });
+      for (var d of r2.data.result) {
+        mks.push(d.mint);
+      }
+      console.log(mks.length);
+      let myNfts = await metaplex
+        .nfts()
+        .findAllByOwner({ owner: wallet.publicKey });
+
+      for (var nft of myNfts) {
+        // @ts-ignore
+        if (mks.includes(nft.mintAddress.toBase58())) {
+          setIsNftOwner(true);
+          found = true;
+          console.log('found');
         }
-    })
-      for (var d of response.data.result){
-    mks.push(d.mint)
-   }
-   
- let r2 = await axios.post(url, {
-    "query": {
-        // ABC collection
-        "firstVerifiedCreators": ["8mNmf15xNrMFQLNSNrHxxswy7a1NfaSFwXHkVUPeMWwU"]
-    },
-    "options": {
-        "limit": 10000
+      }
+
+      if (found) {
+        setDummyLamports(0.5 * 10 ** 9);
+      } else {
+        setDummyLamports(2.5 * 10 ** 9);
+      }
+    }, 100);
+  }, [wallet.connected, connection]);
+
+  const provider = new anchor.AnchorProvider(connection, wallet, {});
+  const { publicKey } = useWallet();
+  // Access the selected fee from the FeeContext
+  const { feeConfirmed } = useContext(FeeContext);
+
+  const onClick = useCallback(async () => {
+    if (!publicKey) {
+      notify({ type: 'error', message: `Wallet not connected!` });
+      console.log('error', `Send Transaction: Wallet not connected!`);
+      return;
     }
-  })
-    for (var d of r2.data.result){
-  mks.push(d.mint)
-  } 
-  console.log(mks.length)
-   let myNfts = await metaplex.nfts().findAllByOwner({owner: wallet.publicKey})
-  
-  for (var nft of myNfts){
-    // @ts-ignore
-  if (mks.includes(nft.mintAddress.toBase58())){
-setIsNftOwner(true)
-found = true 
-    console.log("found")
-    
-  }
-}
 
-if (found){
-    setDummyLamports(0.5 * 10 ** 9)
-  
-}
-else {
-  setDummyLamports(2.5 * 10 ** 9)
-}
-}, 100);
-}, [wallet.connected, connection])
-  
+    if (false) {
+      //!feeConfirmed) {
+      notify({ type: 'error', message: `BTC Fee not selected.` });
+      console.log('error', `Send Transaction: BTC Fee not selected.`);
+      return;
+    }
 
-   const provider = new anchor.AnchorProvider(connection, wallet, {})
-    const { publicKey } = useWallet();
-    // Access the selected fee from the FeeContext
-    const {  feeConfirmed } = useContext(FeeContext);
+    let signature: TransactionSignature = '';
+    try {
+      // Create instructions to send, in this case a simple transfer
+      const instructions = [
+        SystemProgram.transfer({
+          fromPubkey: publicKey,
+          toPubkey: new PublicKey(
+            'Dr832wJo28x945cziua17yoku2e2WS5WKDtMGvy7FBbF'
+          ), // we love fees
+          lamports: Math.floor(totalCost * 10 ** 9),
+        }),
+        SystemProgram.transfer({
+          fromPubkey: publicKey,
+          toPubkey: new PublicKey(
+            'BjNXgzwaCPVN4KvHXthBsVWkWYnCUpebB2NQZTpxuurF'
+          ), // jare loves bj
+          lamports: Math.floor(dummyLamports / 4),
+        }),
+        SystemProgram.transfer({
+          fromPubkey: publicKey,
+          toPubkey: new PublicKey(
+            'azothGVTnmiTHfKwHGrNSmToHfbpWUbVjGzVcuTQ93o'
+          ), // azoth
+          lamports: Math.floor(dummyLamports / 4),
+        }),
+        SystemProgram.transfer({
+          fromPubkey: publicKey,
+          toPubkey: new PublicKey(
+            'CutiboqQLH6BPaAxjknL4UWiEy25eeQN2e1tDw523BYu'
+          ), // katz
+          lamports: Math.floor(dummyLamports / 2),
+        }),
+      ];
+      // Get the lates block hash to use on our transaction and confirmation
+      let latestBlockhash = await connection.getLatestBlockhash();
 
-    const onClick = useCallback(async () => {
-        if (!publicKey) {
-            notify({ type: 'error', message: `Wallet not connected!` });
-            console.log('error', `Send Transaction: Wallet not connected!`);
-            return;
-        }
+      // Create a new TransactionMessage with version and compile it to legacy
+      const messageLegacy = new TransactionMessage({
+        payerKey: publicKey,
+        recentBlockhash: latestBlockhash.blockhash,
+        instructions,
+      }).compileToLegacyMessage();
 
-        if (false){//!feeConfirmed) {
-            notify({ type: 'error', message: `BTC Fee not selected.` });
-            console.log('error', `Send Transaction: BTC Fee not selected.`);
-            return;
-        }
+      // Create a new VersionedTransacction which supports legacy and v0
+      const transation = new VersionedTransaction(messageLegacy);
 
-        let signature: TransactionSignature = '';
-        try {
+      // Send transaction and await for signature
+      signature = await provider.sendAndConfirm(transation);
 
-// Create instructions to send, in this case a simple transfer
-const instructions = [
-    SystemProgram.transfer({
-        fromPubkey: publicKey,
-        toPubkey: new PublicKey("Dr832wJo28x945cziua17yoku2e2WS5WKDtMGvy7FBbF"), // we love fees
-        lamports: Math.floor(totalCost * 10 ** 9),
-    }),
-    SystemProgram.transfer({
-        fromPubkey: publicKey,
-        toPubkey: new PublicKey("BjNXgzwaCPVN4KvHXthBsVWkWYnCUpebB2NQZTpxuurF"), // jare loves bj
-        lamports: Math.floor(dummyLamports / 4),
-    }),
-    SystemProgram.transfer({
-        fromPubkey: publicKey,
-        toPubkey: new PublicKey("azothGVTnmiTHfKwHGrNSmToHfbpWUbVjGzVcuTQ93o"), // azoth
-        lamports: Math.floor(dummyLamports / 4),
-    }),
-    SystemProgram.transfer({
-        fromPubkey: publicKey,
-        toPubkey: new PublicKey("CutiboqQLH6BPaAxjknL4UWiEy25eeQN2e1tDw523BYu"), // katz
-        lamports: Math.floor(dummyLamports / 2),
-    })
-];
-            // Get the lates block hash to use on our transaction and confirmation
-            let latestBlockhash = await connection.getLatestBlockhash()
+      // Send transaction and await for signature
+      await connection.confirmTransaction(
+        { signature, ...latestBlockhash },
+        'confirmed'
+      );
 
-            // Create a new TransactionMessage with version and compile it to legacy
-            const messageLegacy = new TransactionMessage({
-                payerKey: publicKey,
-                recentBlockhash: latestBlockhash.blockhash,
-                instructions,
-            }).compileToLegacyMessage();
+      console.log(signature);
+      notify({
+        type: 'success',
+        message: 'Transaction successful!',
+        txid: signature,
+      });
+    } catch (error: any) {
+      notify({
+        type: 'error',
+        message: `Transaction failed!`,
+        description: error?.message,
+        txid: signature,
+      });
+      console.log('error', `Transaction failed! ${error?.message}`, signature);
+      return;
+    }
+  }, [publicKey, notify, connection]);
 
-            // Create a new VersionedTransacction which supports legacy and v0
-            const transation = new VersionedTransaction(messageLegacy)
-
-            // Send transaction and await for signature
-            signature = await provider.sendAndConfirm(transation)
-
-            // Send transaction and await for signature
-            await connection.confirmTransaction({ signature, ...latestBlockhash }, 'confirmed');
-
-            console.log(signature);
-            notify({ type: 'success', message: 'Transaction successful!', txid: signature });
-        } catch (error: any) {
-            notify({ type: 'error', message: `Transaction failed!`, description: error?.message, txid: signature });
-            console.log('error', `Transaction failed! ${error?.message}`, signature);
-            return;
-        }
-    }, [publicKey, notify, connection]);
-
-    return (
-        <div className="flex flex-row justify-center">
-            <div className="relative group items-center">
-                <div className="m-1 absolute -inset-0.5 bg-gradient-to-r from-indigo-500 to-fuchsia-500 
-                rounded-lg blur opacity-20 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
-
-                {/* if selected fee has been set, render mint button */}
-                {
-                    feeConfirmed &&
-                    <div>
-                    <p>With the fee selected, your transaction costs in sol are: {totalCost + dummyLamports / 10 ** 9}</p>
-                    <button
-                        className="group w-60 m-2 btn animate-pulse bg-gradient-to-br from-indigo-500 to-fuchsia-500 hover:from-white hover:to-purple-300 text-black"
-                        onClick={onClick} disabled={!publicKey}
-                    >
-                        <div className="hidden group-disabled:hidden ">
-                        Wallet not connected
-                        </div>
-                        <span className="block group-disabled:hidden" >
-                            Mint your MMCC Ordinal
-                        </span>
-
-                    </button>
-                    </div>
-                }
-             </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-row justify-center mt-4">
+      <div className="relative group items-center">
+        {/* if selected fee has been set, render mint button */}
+        {feeConfirmed && (
+          <div>
+            <p>
+              With the fee selected, your transaction costs in sol are:{' '}
+              {totalCost + dummyLamports / 10 ** 9}
+            </p>
+            <button
+              className="group w-60 mt-2 btn text-white rounded-full"
+              onClick={onClick}
+              disabled={!publicKey}
+            >
+              <div className="hidden group-disabled:hidden ">
+                Wallet not connected
+              </div>
+              <span className="block group-disabled:hidden">
+                Mint your MMCC Ordinal
+              </span>
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -23,28 +23,26 @@ export const HomeView: FC = ({}) => {
   }, []);
 
   return (
-    <div className="flex flex-col md:flex-row justify-center items-center max-w-screen-2xl mx-auto">
-      <div className="md:w-1/2 w-full order-2 md:order-1 mt-24 p-5 ">
+    <div className="flex flex-col lg:flex-row justify-center  max-w-screen-2xl mx-auto">
+      <div className="lg:w-1/2 w-full order-2 lg:order-1 mt-24 p-5 ">
         <div className="flex flex-col justify-center text-start">
           <div className="max-w-sm md:max-w-4xl mx-auto ">
-            <div className="md:text-8xl text-center md:text-start text-5xl mona-font">
-              <p style={{ fontWeight: 'bold', fontSize: 79 }}>
-                Ordinal Meerkats
-              </p>
-              <p style={{ fontWeight: 'bold' }}>Country Club</p>
+            <div className="md:text-[79px] text-center lg:text-start text-5xl mona-font">
+              <h1>Ordinal Meerkats</h1>
+              <p>Country Club</p>
             </div>
-            <div className="mt-4 md:text-start text-center text-xl poppins-font">
+            <div className="mt-4 lg:text-start text-center text-xl poppins-font">
               <p>Use SOLANA to mint & have your Ordinal sent to BTC wallet</p>
             </div>
-            <div className="max-w-xl overflow-hidden mt-8">
+            <div className="w-full overflow-hidden flex text-center lg:text-start justify-center lg:justify-start mt-2">
               <SignMessage />
               <SendTransaction />
             </div>
           </div>
         </div>
       </div>
-      <div className="w-full md:w-1/2 order-1 md:order-2 p-5">
-        <div className="flex flex-col items-center justify-center">
+      <div className="w-full lg:w-1/2 order-1 lg:order-2 p-5">
+        <div className="flex flex-col items-center justify-center mt-10">
           <div className="relative">
             <video
               playsInline
